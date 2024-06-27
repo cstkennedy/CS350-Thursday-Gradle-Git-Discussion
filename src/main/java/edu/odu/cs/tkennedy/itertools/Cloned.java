98fbb546c8fa4bcc13556c3c8df7b4a518f5d3b8
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-06-27 17:10:54 -0400

    Write basic cloned interface and test

---
+++ src/main/java/edu/odu/cs/tkennedy/itertools/Cloned.java
@@ -0,0 +1,11 @@
+package edu.odu.cs.tkennedy.itertools;
+
+import java.util.Iterator;
+
+public class Cloned
+{
+    public static <T extends Cloneable> Iterable<T> cloned(Iterable<T> sourceCollection)
+    {
+        return null;
+    }
+}
